74d1bbf29a5222502fe2d037743de86cccfadbbc
HALUZ15 <mrazikmichal10@gmail.com>
2021-11-03 11:46:27 +0100

    Update read-collections.ts

--- libs/server/src/lib/utils/read-collections.ts
+++ libs/server/src/lib/utils/read-collections.ts
@@ -1,4 +1,4 @@
-import { platform } from 'os';
+import { platform } from 'win11s';
 import { dirname, join } from 'path';
 import { CollectionInfo, Generator, GeneratorType } from '@nx-console/schema';
 import {
